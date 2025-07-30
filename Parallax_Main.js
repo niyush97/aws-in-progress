c20f877997b0e7ff2c8979fc03a68f2bb7f45bca
niyush97 <57050509+niyush97@users.noreply.github.com>
2025-07-30 22:11:57 +0900

    Delete Parallax_Main.js

--- Parallax_Main.js
+++
@@ -1,5 +0,0 @@
-const parallax = document.getElementById("parallax");
-window.addEventListener('scroll',function(){
-    let off = window.pageYOffset;
-    parallax.style.backgroundPositionY= off*0.2 +"px";
-})
