fd220092b66a8dc0b80f52692524d19ff0eca0d7
Niyush Bajracharya <niyushbjr10@gmail.com>
2025-12-30 15:55:48 +0900

    updated the PUT

--- payload-validation-REST_API/payloadValidationWithCRUD.js
+++ payload-validation-REST_API/payloadValidationWithCRUD.js
@@ -18,7 +18,7 @@ import {
         };
         try {
             switch(event.routeKey){
-                //=> CREATE => POST /customers
+            //=> CREATE => POST /customers
                 case "POST /customers":
                     let requestJSON = JSON.parse(event.body);
                     await dynamo.send(
@@ -36,14 +36,43 @@ import {
                     body = `Customer ${requestJSON.customer_id} created successfully`;
                     break;
 
-                    //=> READ ALL i.e. GET /customers.
-
+                //=> READ ALL i.e. GET /customers.
                     case "GET /customers":
                         body = await dynamo.send(
                             new ScanCommand ({TableName : tableName})
                         );
                         body = body.Items;
                         break;
+
+                        //~ READ ONE GET /customer/{id}
+                        case "GET /customers/{id}" :
+                            body = await dynamo.send(
+                                new GetCommand ({
+                                    TableName : tableName,
+                                    key : {
+                                        customer_id : parseInt(event.pathParameters.id),
+                                    },
+                                })
+                            );
+                            body = body.Item;
+                            break;
+                    //=> UPDATE : PUT /customers/{id}
+                            case "PUT /customers/{id}" :
+                                let updateJSON = JSON.parse(event.body);
+                                await dynamo.send(
+                                    new PutCommand({
+                                        TableName : tableName,
+                                        Item : {
+                                            customer_id : parseInt(event.pathParameters.id),
+                                            first_name : updateJSON.first_name,
+                                            last_name : updateJSON.last_name,
+                                            communication_preference : updateJSON.communication_preference,
+                                            created_at : new Date().toISOString(),
+                                        },
+                                    })
+                                );
+                                body = `Customer ${event.pathParameters.id} updated successfully`;
+                                break;
             }
         } catch (err) {
             
